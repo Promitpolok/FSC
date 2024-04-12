924fb1b14b47d36c2e312e8a613a94774d620a33
tanzimaiub <149145114+tanzimaiub@users.noreply.github.com>
2024-04-12 18:58:20 +0600

    5th commit

--- src/Buyer/MembershipFormController.java
+++ src/Buyer/MembershipFormController.java
@@ -5,13 +5,20 @@
 package Buyer;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 /**
  * FXML Controller class
@@ -21,10 +28,6 @@ import javafx.scene.control.TextField;
 public class MembershipFormController implements Initializable {
 
     @FXML
-    private RadioButton SilverMembershipRadioButton;
-    @FXML
-    private RadioButton PlatinumMembershipRadioButton;
-    @FXML
     private TextField NameTextField;
     @FXML
     private TextField IDTextField;
@@ -32,6 +35,10 @@ public class MembershipFormController implements Initializable {
     private TextField AddressTextField;
     @FXML
     private Label ShowMembershipFeesLabel;
+    @FXML
+    private ComboBox<String> MembershipTypeComboBox;
+    
+    ArrayList<Members> MembershipList;
 
     /**
      * Initializes the controller class.
@@ -39,14 +46,29 @@ public class MembershipFormController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        MembershipTypeComboBox.getItems().addAll("Silver","Platinum");
+        MembershipList = new ArrayList<Members>();
+        ShowMembershipFeesLabel.setText("Silver: 1000tk, Platinum: 1500tk");
+        
     }    
 
     @FXML
     private void SubmitMembershipOnClick(ActionEvent event) {
+         MembershipList.add(
+            new Member(
+                Integer.parseInt(IDTextField.getText()),
+                NameTextField.getText(),
+                MembershipTypeComboBox.getValue(),
+                AddressTextField.getText()
+            )
+        );
+        IDTextField.clear(); NameTextField.clear();
+        AddressTextField.clear();
     }
 
     @FXML
     private void GoBackOnClick(ActionEvent event) {
+        
     }
     
 }
