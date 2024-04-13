fc3043a93d20bba653bdd147fbb88608e0430c49
JamiulIslamNahin <157962682+JamiulIslamNahin@users.noreply.github.com>
2024-04-13 16:38:11 +0600

    update Digital Marketing Executive

---
+++ src/Digital_Marketing_Excutive/AdvertisementController.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package Digital_Marketing_Excutive;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author USER
+ */
+public class AdvertisementController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void backButtonMouseOnClick(ActionEvent event) throws IOException { 
+                Parent root = null;
+        FXMLLoader myLoader = new FXMLLoader(getClass().getResource("Digital_Marketing_Excutive.fxml"));
+root = (Parent) myLoader.load();
+Scene myScene = new Scene(root);
+
+Digital_Marketing_ExcutiveController x = myLoader.getController();
+//x.setValue(value);
+
+Stage myStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+myStage.setScene(myScene);
+myStage.show();  
+    }
+
+    @FXML
+    private void createAdsButtonOnClick(ActionEvent event) throws IOException { 
+                Parent root = null;
+        FXMLLoader myLoader = new FXMLLoader(getClass().getResource("CreateAds.fxml"));
+root = (Parent) myLoader.load();
+Scene myScene = new Scene(root);
+
+CreateAdsController x = myLoader.getController();
+//x.setValue(value);
+
+Stage myStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+myStage.setScene(myScene);
+myStage.show();  
+    }
+
+    @FXML
+    private void viewAdsButtonOnClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void deleteAdsButtonMouseOnClick(ActionEvent event) {
+    }
+    
+}
